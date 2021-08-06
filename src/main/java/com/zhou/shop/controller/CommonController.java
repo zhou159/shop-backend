dce8ec6fb0a9d16c66199900680f8469779b0052
zhouxiong <1595904478@qq.com>
2021-08-06 19:40:22 +0800

    新增了一个上传图片的公共接口

--- src/main/java/com/zhou/shop/controller/CommonController.java
+++ src/main/java/com/zhou/shop/controller/CommonController.java
@@ -5,6 +5,8 @@ import com.zhou.shop.result.RestResponse;
 import com.zhou.shop.util.MinioUtil;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 /**
@@ -13,6 +15,8 @@ import org.springframework.web.multipart.MultipartFile;
  * @author 周雄
  * @date 2021年8月5日 21点26分
  */
+@RestController
+@RequestMapping("/common")
 public class CommonController {
     final MinioUtil minioUtil;
 
--- src/main/java/com/zhou/shop/controller/UpdateLogController.java
+++ src/main/java/com/zhou/shop/controller/UpdateLogController.java
@@ -4,16 +4,13 @@ import com.zhou.shop.entity.UpdateLog;
 import com.zhou.shop.result.RestObject;
 import com.zhou.shop.result.RestResponse;
 import com.zhou.shop.service.IUpdateLogService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.util.List;
 
 /**
- * <p>
- *  前端控制器
- * </p>
+ * 前端控制器
  *
  * @author 周雄
  * @since 2021-07-24
@@ -22,58 +19,55 @@ import java.util.List;
 @RequestMapping("/updateLog")
 public class UpdateLogController {
 
-    @Autowired
-    IUpdateLogService iUpdateLogService;
+    final IUpdateLogService iUpdateLogService;
 
-
+    public UpdateLogController(IUpdateLogService iUpdateLogService) {
+        this.iUpdateLogService = iUpdateLogService;
+    }
 
     @PostMapping("/createUpdateLog")
-    public RestObject<String> createUpdateLog(@RequestBody UpdateLog updateLog){
+    public RestObject<String> createUpdateLog(@RequestBody UpdateLog updateLog) {
         updateLog.setUpdateLogCreateTime(LocalDate.now());
         boolean save = iUpdateLogService.save(updateLog);
-        if (save){
+        if (save) {
             return RestResponse.makeOkRsp("新增成功！");
-        }else {
+        } else {
             return RestResponse.makeErrRsp("新增成功！");
         }
     }
 
     @GetMapping("/retrieveByUpdateLogId/{updateLogId}")
-    public RestObject<UpdateLog> retrieveByUpdateId(@PathVariable int updateLogId){
+    public RestObject<UpdateLog> retrieveByUpdateId(@PathVariable int updateLogId) {
         UpdateLog updateLog = iUpdateLogService.getById(updateLogId);
         return RestResponse.makeOkRsp(updateLog);
     }
 
     @GetMapping("/retrieveAllUpdateLog")
-    public RestObject<List<UpdateLog>> retrieveAllUpdateLog (){
+    public RestObject<List<UpdateLog>> retrieveAllUpdateLog() {
         List<UpdateLog> list = iUpdateLogService.list();
         return RestResponse.makeOkRsp(list);
     }
 
     @PostMapping("/updateUpdateByUpdateLogId/{updateLogId}")
-    public RestObject<String> updateUpdateByUpdateId(@PathVariable int updateLogId,@RequestBody UpdateLog updateLog){
+    public RestObject<String> updateUpdateByUpdateId(
+            @PathVariable int updateLogId, @RequestBody UpdateLog updateLog) {
         updateLog.setUpdateLogId(updateLogId);
         updateLog.setUpdateLogCreateTime(LocalDate.now());
         boolean b = iUpdateLogService.updateById(updateLog);
-        if (b){
+        if (b) {
             return RestResponse.makeOkRsp("修改成功！");
-        }else {
+        } else {
             return RestResponse.makeErrRsp("修改失败！");
         }
     }
 
     @PostMapping("/deleteByUpdateLogId/{updateLogId}")
-    public RestObject<String> deleteUpdateById(@PathVariable int updateLogId){
+    public RestObject<String> deleteUpdateById(@PathVariable int updateLogId) {
         boolean b = iUpdateLogService.removeById(updateLogId);
-        if (b){
+        if (b) {
             return RestResponse.makeOkRsp("删除成功！");
-        }else {
+        } else {
             return RestResponse.makeErrRsp("删除失败！");
         }
     }
-
-
-
-
-
 }
